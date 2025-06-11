424fba9426794166c2e282ef83d4f356cf62f7e3
Julienpetit951 <julienpetit951@gmail.com>
2025-06-11 20:02:58 +0200

    Header integration
    Header integration

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <h1>Bonjour {{ name }}</h1>
-  <p>Commentaire</p>
+  <header>
+    <div class="logo"><img src="./assets/icons/logo.svg" alt="Picto JP"></div>
+    <div class="menu">
+      <ul>
+        <li>A propos</li>
+        <li>Compétences</li>
+        <li>Projets</li>
+     </ul>
+    </div>
+  <div class="contact">
+    <LinkedinIcon />
+    <GithubIcon />
+    <CvIcon />
+  </div>
+  </header>
 </template>
 
 <script setup>
-const name = 'John Doe'
+import LinkedinIcon from '@/assets/icons/linkedin.svg'
+import CvIcon from '@/assets/icons/cv.svg'
+import GithubIcon from '@/assets/icons/github.svg'
 </script>
+
+
+<style>
+</style>
