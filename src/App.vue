6d198c50c45c88dc2d4122997984133cb459ed3b
Julienpetit951 <julienpetit951@gmail.com>
2025-06-12 14:42:54 +0200

    Implementation Header
    Implementation Header

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <template>
-  <header>
-    <div class="logo"><img src="./assets/icons/logo.svg" alt="Picto JP"></div>
-    <div class="menu">
-      <ul>
-        <li>A propos</li>
-        <li>Compétences</li>
-        <li>Projets</li>
-     </ul>
-    </div>
-  <div class="contact">
-    <LinkedinIcon class="icon-size" />
-    <GithubIcon class="icon-size" />
-    <a href="#">CV</a>
-  </div>
-  </header>
+  <Header />
+  <Footer />
 </template>
 
-<style scoped>
-.icon-size {
-  width: 60px;
-  height: 60px;
-}
-</style>
-
 <script setup>
-import LinkedinIcon from '@/assets/icons/linkedin.svg'
-import GithubIcon from '@/assets/icons/github.svg'
-import HtmlIcon from '@/assets/icons/html5.svg'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
-
-<style>
-.icon-size {
-  width: 80px;
-  height: 80px;
-}
+<style lang="scss">
+@use './styles/main.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+      <header>
+      <div class="logo"><img src="../assets/icons/logo.svg" alt="Picto JP"></div>
+      <div class="menu">
+        <ul>
+          <li>A propos</li>
+          <li>Compétences</li>
+          <li>Projets</li>
+        </ul>
+      </div>
+      <div class="contact">
+        <a href="https://www.linkedin.com/in/%F0%9F%9A%80-julien-petit-182580266/" target="_blank"><LinkedinIcon class="icon-size" /></a>
+        <a href="https://github.com/Scofrard" target="_blank"><GithubIcon class="icon-size" /></a>
+        <a href="https://github.com/Scofrard" target="_blank">CV</a>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup>
+import LinkedinIcon from '@/assets/icons/linkedin.svg'
+import GithubIcon from '@/assets/icons/github.svg'
+import HtmlIcon from '@/assets/icons/html5.svg'
+</script>
+
+
+<style lang="scss">
+@use '../styles/header.scss';
+</style>
